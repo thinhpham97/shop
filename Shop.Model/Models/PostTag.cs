3efc67aae3e217468befd5b270a2ead44c071374
DESKTOP-6BLPA2S\phamt <phamloimb@gmail.com>
2017-11-25 17:12:00 +0700

    add domain entities for project

---
+++ Shop.Model/Models/PostTag.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shop.Model.Models
+{
+    public class PostTag
+    {
+        [Key]
+        [MaxLength(50)]
+        public int PostID { set; get; }
+
+        [Key]
+        [Column(TypeName = "varchar")]
+        [MaxLength(50)]
+        public string TagID { set; get; }
+
+        [ForeignKey("PostID")]
+        public virtual Post Post { set; get; }
+
+        public virtual Tag Tag { set; get; }
+    }
+}
---
+++ Shop.Model/Models/SupportOnline.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shop.Model.Models
+{
+    [Table("SupportOnlines")]
+    public class SupportOnline
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { set; get; }
+
+        [MaxLength(256)]
+        public string Department { set; get; }
+
+        [MaxLength(256)]
+        public string Skype { set; get; }
+
+        [MaxLength(256)]
+        public string Mobile { set; get; }
+
+        [MaxLength(256)]
+        public string Email { set; get; }
+
+        [MaxLength(256)]
+        public string Facebook { set; get; }
+
+        [MaxLength(256)]
+        public string TeamViewer { set; get; }
+
+        [MaxLength(50)]
+        public bool Status { set; get; }
+
+        public int? DisplayOrder { set; get; }
+    }
+}
